e57c1630434bffbc12ceea41d30d348be7de05a6
karl-thomas <karl.thomas925@gmail.com>
2024-01-01 03:43:24 -0600

    create configuration file to set port/db settings - set up files a little better and update imports - write initial case in tests for seeing if the db has the user thats created, this will fail right now

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,11 @@
 //! tests/health_check.rs
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
     // this is the only app specific line
     let address = spawn_app();
-
     let client = reqwest::Client::new();
 
     let response = client
@@ -20,6 +21,12 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -32,6 +39,15 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gamil.com");
+    assert_eq!(saved.name, "le guin");
+
 }
 
 #[tokio::test]
@@ -65,7 +81,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to listen to address");
+    let server = zero2prod::startup::run(listener).expect("Failed to listen to address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
