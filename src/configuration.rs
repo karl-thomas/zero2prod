3744e56547c881823febcea952aca29ea35e2921
karl-thomas <karl.thomas925@gmail.com>
2024-02-04 00:58:03 -0600

    use a pool for the datbase connection, add to app context data

--- src/configuration.rs
+++ src/configuration.rs
@@ -25,13 +25,19 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Try to convert the configuration values it read into
     // our Settings type
     settings.try_deserialize::<Settings>()
-} 
+}
 
 impl DatabaseSettings {
-  pub fn connection_string(&self) -> String {
-    format!(
-      "postgres://{}:{}@{}:{}/{}",
-      self.username, self.password, self.host, self.port, self.database_name
-    )
-  }
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,10 @@
-use actix_web::{web::Form, HttpResponse};
+use actix_web::{
+    web::{self, Form},
+    HttpResponse,
+};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +12,26 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_user: Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(user: Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        user.email,
+        user.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) =>  HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+
+    }
+
 }
